bf599edf596c9575bd887d29f35c8a49c6de5a8c
TSV Kali prasad K <kaliprasad4035@gmail.com>
2022-11-05 10:11:57 +0530

    my first npm

---
+++ index.js
@@ -0,0 +1,18 @@
+function kvp(input,target)
+{
+    for(let key in input)
+    {
+        let value=input[key]
+        if(key ===target){
+            return value
+        }
+        let type=typeof value
+        if(type==="object"||type==="array"){
+            let output=findDee(value,target)
+            if(output){
+                return output
+            }
+        }
+              
+    }
+}
